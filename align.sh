5009cc741f1385160afe7eae2d4f2d28db046848
ankur <s.ankursonawane@gmail.com>
2020-07-01 18:05:41 +0530

    Align all

--- align.sh
+++ align.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-
 set -e
 set -x
 
 source ./config.sh
 
+
 rm -rf data_align
+
+for trainpref in 'data/train_merge' 'data/valid' 'data/test';
+do
 mkdir data_align
-
-
-trainpref='data/train_merge'
-#trainpref='data/valid'
 
 python scripts/build_sym_alignment.py --fast_align_dir ./software/fast_align/build/ \
 --mosesdecoder_dir ./software/mosesdecoder --source_file $trainpref.src \
@@ -21,3 +20,4 @@ cp data_align/align.forward $trainpref.forward
 cp data_align/align.backward $trainpref.backward
 
 rm -rf data_align
+done
